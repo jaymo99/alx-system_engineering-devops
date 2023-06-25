94479f055ed540c487dfcc5f743b8cee824ba671
James Kariuki <karisjaymo99@gmail.com>
2023-06-25 22:21:22 +0300

    update 'unknown resource type' file_line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,15 @@
 # edit SSH config file
 
 file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthenticaton no',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     PasswordAuthenticaton no',
+  replace => true,
 }
 
 file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-}
-
-service {'ssh':
-  ensure => running,
-  enable => true,
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
