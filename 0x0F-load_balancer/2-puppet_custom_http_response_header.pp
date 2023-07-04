27e5b16cbd762f37868388f56fcaf32c02a19b1e
James Kariuki <karisjaymo99@gmail.com>
2023-07-04 22:55:34 +0300

    refactor '2-puppet_custom_http_response_header.pp'

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,6 @@
 # Installation and Configuration of Nginx in Ubuntu
-exec { 'update':
-  command => '/usr/bin/env apt-get -y update',
-}
-
 package { 'nginx' :
-  ensure  => installed,
-  require => Exec['update'],
-}
-
-# Add a custom HTTP header
-file_line { 'http_header':
-  path    => '/etc/nginx/nginx.conf',
-  after   => 'http {',
-  line    => "\tadd_header X-Served-By ${hostname};",
-  require => Package['nginx'],
+  ensure => installed,
 }
 
 # Create index.html file
@@ -21,6 +8,15 @@ file { 'index.html' :
   ensure  => present,
   path    => '/var/www/html/index.html',
   content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Add a custom HTTP header
+file_line { 'custom_http_header':
+  path    => '/etc/nginx/nginx.conf',
+  after   => 'server_name _;',
+  line    => "\tadd_header X-Served-By \$hostname;",
+  notify  => Exec['restart_nginx'],
   require => Package['nginx'],
 }
 
@@ -35,7 +31,7 @@ file { '404.html' :
 file_line { '404_error_page':
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;',
-  line    => 'error_page 404 /404.html;',
+  line    => "\terror_page 404 /404.html;",
   notify  => Exec['restart_nginx'],
   require => File['404.html'],
 }
@@ -44,7 +40,7 @@ file_line { '404_error_page':
 file_line { 'nginx-redirection-rule':
   path    => '/etc/nginx/sites-available/default',
   after   => 'server_name _;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line    => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
   notify  => Exec['restart_nginx'],
   require => File['index.html'],
 }
