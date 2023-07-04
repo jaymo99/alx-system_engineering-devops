1fb12f8df821665998c50222113380531011a398
James Kariuki <karisjaymo99@gmail.com>
2023-07-05 00:00:04 +0300

    add copy of 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,6 +8,15 @@ package { 'nginx' :
   require => Exec['update'],
 }
 
+# Add a custom HTTP header
+file_line { 'custom_http_header':
+  path    => '/etc/nginx/nginx.conf',
+  after   => 'http {',
+  line    => 'add_header X-Served-By $hostname;',
+  notify  => Exec['restart_nginx'],
+  require => Package['nginx'],
+}
+
 # Create index.html file
 file { 'index.html' :
   ensure  => present,
@@ -16,13 +25,29 @@ file { 'index.html' :
   require => Package['nginx'],
 }
 
-# Add a custom HTTP header
-file_line { 'custom_http_header':
-  path    => '/etc/nginx/nginx.conf',
-  after   => 'http {',
-  line    => 'add_header X-Served-By $hostname;',
-  notify  => Exec['restart_nginx'],
+# Create custom 404.html file
+file { '404.html' :
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page',
   require => Package['nginx'],
+}
+
+file_line { '404_error_page':
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'error_page 404 /404.html;',
+  notify  => Exec['restart_nginx'],
+  require => File['404.html'],
+}
+
+# Add redirection rule
+file_line { 'nginx-redirection-rule':
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  notify  => Exec['restart_nginx'],
+  require => File['index.html'],
 }
 
 # NGINX service
@@ -36,5 +61,4 @@ service { 'nginx':
 exec { 'restart_nginx':
   command     => '/usr/sbin/service nginx restart',
   refreshonly => true,
-  require => Package['nginx'],
 }
