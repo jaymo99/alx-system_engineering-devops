c86024ce454f08a739b1dc9f6925e6f2e69c8bd1
James Kariuki <karisjaymo99@gmail.com>
2023-07-04 21:24:33 +0300

    add puppet manifest to initialize ubuntu server with nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,48 @@
+# Installation and Configuration of Nginx in Ubuntu
+package { 'nginx' :
+  ensure => installed,
+}
+
+# Create index.html file
+file { 'index.html' :
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
+}
+
+# Create custom 404.html file
+file { '404.html' :
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page',
+}
+
+file_line { '404_error_page':
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'error_page 404 /404.html;',
+  notify  => Exec['restart_nginx'],
+  require => File['404.html'],
+}
+
+# Add redirection rule
+file_line { 'nginx-redirection-rule':
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'server_name _;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  notify  => Exec['restart_nginx'],
+  require => File['index.html'],
+}
+
+# NGINX service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Restart NGINX service
+exec { 'restart_nginx':
+  command     => 'service nginx restart',
+  refreshonly => true,
+}
