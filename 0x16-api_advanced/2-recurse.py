f3461b38151b376af0983ccdf830b2d2ed81a638
James Kariuki <karisjaymo99@gmail.com>
2023-08-08 21:44:34 +0300

    count all hot articles for a subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines function that uses Reddit API pagination
+"""
+
+import requests
+
+base_url = "https://www.reddit.com"
+route = "/r/{}/hot.json"
+headers = {
+        "User-Agent": "Alx_tasks"
+        }
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """returns a list containing all hot article titles for a subreddit
+    """
+    filters = "?limit=100"
+    if after:
+        filters += "&after={}".format(after)
+
+    url = base_url + route.format(subreddit) + filters
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    titles = []
+    if (response.status_code == 200):
+        data = response.json()['data']
+        titles = [post['data']['title'] for post in data['children']]
+        hot_list.extend(titles)
+
+        if data['after']:
+            recurse(subreddit, hot_list, data['after'])
+    return hot_list
