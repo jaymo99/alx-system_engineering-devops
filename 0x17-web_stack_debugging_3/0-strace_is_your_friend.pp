7aad7fb3568e15cfe8545ceda0ab08da5275bccc
James Kariuki <karisjaymo99@gmail.com>
2023-08-11 14:43:01 +0300

    use 'g' option in puppet sed command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Fix file typo in Wordpress settings file
 exec { 'fix_settings_file':
-  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
