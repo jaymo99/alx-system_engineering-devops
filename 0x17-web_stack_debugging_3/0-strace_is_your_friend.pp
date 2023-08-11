e4b2259c722f754a3b4f614fb7525349101d7238
James Kariuki <karisjaymo99@gmail.com>
2023-08-11 14:47:50 +0300

    run sed command with sudo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fix file typo in Wordpress settings file
+
 exec { 'fix_settings_file':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path 	  => '/usr/local/bin/:/bin/'
+  command => 'sudo sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
